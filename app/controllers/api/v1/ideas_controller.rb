73678dcb5e023683e9a292636089bd4a48b0c620
Scott Regenthal <scottregenthal@gmail.com>
2020-02-23 16:48:05 -0700

    Add create action for idea

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -10,5 +10,28 @@ class Api::V1::IdeasController < ApplicationController
     render json: IdeaSerializer.new(Idea.find(params[:id]))
   end
 
-end
+  def create
+    nineteenohnine = Cohort.create!(cohort_number: '1909')
+    user = User.create!(cohort_id: nineteenohnine.id, github_id: '1', token: '1', handle: 'a')
 
+    Idea.create!(title: params[:title], pitch: params[:pitch], problem: params[:problem], solution: params[:solution], features: params[:features], audience: params[:audience], apis: params[:apis], oauth: params[:OAuth], cohort_id: nineteenohnine.id, user_id: user.id)
+    # Idea.create!(idea_params, user: user.id, cohort_id: nineteenohnine.id)
+  end
+
+  private
+
+  def idea_params
+    params.permit(
+      :title,
+      :pitch,
+      :problem,
+      :solution,
+      :audience,
+      :features,
+      :apis,
+      :oauth,
+      :OAuth,
+      :cohort_id
+    )
+  end
+end
