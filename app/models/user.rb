bcf4bd2c5c1864930a4e7d59cd467cab7d811025
Scott Regenthal <scottregenthal@gmail.com>
2020-02-25 15:56:39 -0700

    Add user model
    Co-Authored-By: Harrison <harrison483@gmail.com>
    Co-Authored-By: Joshua O'bannon <jtobannon@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
   validates_presence_of :github_id
   validates_presence_of :token
   validates_presence_of :handle
+
+  def self.create_user(auth_hash)
+    new(
+      github_id: auth_hash[:uid],
+      token: auth_hash[:credentials][:token],
+      handle: auth_hash[:info][:nickname],
+      cohort_id: '1909'
+    )
+  end
 end
