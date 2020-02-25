4c6fe23e964a1afd3d86236a4048560b6221720a
Harrison Levin <harrison483@gmail.com>
2020-02-24 23:55:05 -0700

    controller vote/create

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,14 +1,22 @@
 class Api::V1::IdeasController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def index
-    # cohort = Cohort.find(params[:cohort_id])
-    cohort = Cohort.find_by(cohort_number: params[:cohort_id])
-    render json: IdeaSerializer.new(cohort.ideas)
+    user = User.last
+    #cohort = Cohort.find_by(cohort_number: params[:cohort_id])
+    cohort = Cohort.find(user.cohort_id)
+    render json: IdeaSerializer.new(cohort.ideas.sort_by { |idea| idea.vote_count }.reverse)
   end
 
   def show
     render json: IdeaSerializer.new(Idea.find(params[:id]))
   end
 
-end
+  def create
+    idea = Idea.find(params[:id])
+    idea.vote_count += 1
+    idea.save
+    redirect_to 'http://localhost:9292/cohorts/ideas'
+  end
 
+end
