5db676f988d55fe0d13e35372e87bf3dc8cf4c9b
Harrison Levin <harrison483@gmail.com>
2020-02-24 23:54:40 -0700

    vote count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_003203) do
+ActiveRecord::Schema.define(version: 2020_02_25_054713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_02_22_003203) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "cohort_id", null: false
+    t.integer "vote_count", default: 0
     t.index ["cohort_id"], name: "index_ideas_on_cohort_id"
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
