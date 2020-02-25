2af0499bb833ca5f4442783bfc80dac30557cad6
Scott Regenthal <scottregenthal@gmail.com>
2020-02-25 15:56:53 -0700

    Update controllers
    Co-Authored-By: Harrison <harrison483@gmail.com>
    Co-Authored-By: Joshua O'bannon <jtobannon@gmail.com>

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,8 +2,12 @@ class Api::V1::IdeasController < ApplicationController
 
   def index
     # cohort = Cohort.find(params[:cohort_id])
+    # require "pry"; binding.pry
+    # user = User.find(session[:user_id])
+    # render json: user
     cohort = Cohort.find_by(cohort_number: params[:cohort_id])
     render json: IdeaSerializer.new(cohort.ideas)
+
   end
 
   def show
@@ -11,11 +15,14 @@ class Api::V1::IdeasController < ApplicationController
   end
 
   def create
-    nineteenohnine = Cohort.create!(cohort_number: '1909')
-    user = User.create!(cohort_id: nineteenohnine.id, github_id: '1', token: '1', handle: 'a')
-
-    Idea.create!(title: params[:title], pitch: params[:pitch], problem: params[:problem], solution: params[:solution], features: params[:features], audience: params[:audience], apis: params[:apis], oauth: params[:OAuth], cohort_id: nineteenohnine.id, user_id: user.id)
+    # nineteenohnine = Cohort.create!(cohort_number: '1909')
+    # user = User.create!(cohort_id: nineteenohnine.id, github_id: '1', token: '1', handle: 'a')
+    #
+    # Idea.create!(title: params[:title], pitch: params[:pitch], problem: params[:problem], solution: params[:solution], features: params[:features], audience: params[:audience], apis: params[:apis], oauth: params[:OAuth], cohort_id: nineteenohnine.id, user_id: user.id)
     # Idea.create!(idea_params, user: user.id, cohort_id: nineteenohnine.id)
+
+    session[:user_id] = User.first.id
+    redirect_to 'http://localhost:9292'
   end
 
   private
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    auth_hash = request.env['omniauth.auth']
+    user = User.create_user(auth_hash)
+    session[:user_id] = user.id
+    redirect_to 'http://localhost:9292/cohorts/1909/ideas'
+  end
+end
