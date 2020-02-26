c02a94a0bd48e6be9430ccf852cc128ff7a008b2
Joshua O'bannon <jtobannon@gmail.com>
2020-02-25 18:22:13 -0700

    Create Upsplash Service and Tests

---
+++ spec/services/upsplash_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "UpsplashService can hit upslash API" do
+  it 'if given a keyword of dog, can show a url related to a dog', :vcr do 
+    photo_url = IdeasPhotoGetter.new("dog").get_photo 
+
+    expected_url = "https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjExNzMzMX0"
+    expect(photo_url).to eq(expected_url)
+  end
+end
+
+
