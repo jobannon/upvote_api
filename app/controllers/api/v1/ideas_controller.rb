e2eb8e124a05f7ec9b74f78bd77ead08887dbd63
Scott Regenthal <scottregenthal@gmail.com>
2020-02-25 18:11:53 -0700

    Update index and add post actions within ideas controller
    Co-Authored-By: Harrison <harrison483@gmail.com>
    Co-Authored-By: Joshua O'bannon <jtobannon@gmail.com>

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::IdeasController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    user = User.last
-    #cohort = Cohort.find_by(cohort_number: params[:cohort_id])
+    user = User.find(session[:user_id])
     cohort = Cohort.find(user.cohort_id)
     render json: IdeaSerializer.new(cohort.ideas.sort_by { |idea| idea.vote_count }.reverse)
   end
@@ -13,38 +12,24 @@ class Api::V1::IdeasController < ApplicationController
   end
 
   def create
-    # nineteenohnine = Cohort.create!(cohort_number: '1909')
-    # user = User.create!(cohort_id: nineteenohnine.id, github_id: '1', token: '1', handle: 'a')
-    #
-    # Idea.create!(title: params[:title], pitch: params[:pitch], problem: params[:problem], solution: params[:solution], features: params[:features], audience: params[:audience], apis: params[:apis], oauth: params[:OAuth], cohort_id: nineteenohnine.id, user_id: user.id)
-    # Idea.create!(idea_params, user: user.id, cohort_id: nineteenohnine.id)
-
-    session[:user_id] = User.first.id
-    redirect_to 'http://localhost:9292'
-  end
-
-  def post
-    idea = Idea.find(params[:id])
-    idea.vote_count += 1
-    idea.save
+    user = User.find(session[:user_id])
+    Idea.create!(title: params[:title], pitch: params[:pitch], problem: params[:problem], solution: params[:solution], features: params[:features], audience: params[:audience], apis: params[:apis], oauth: params[:oauth], cohort_id: user.cohort_id, user_id: user.id)
+    # Idea.create!(idea_params, user_id: user.id, cohort_id: user.cohort_id)
     redirect_to 'http://localhost:9292/cohorts/ideas'
   end
 
-
   private
 
-  def idea_params
-    params.permit(
-      :title,
-      :pitch,
-      :problem,
-      :solution,
-      :audience,
-      :features,
-      :apis,
-      :oauth,
-      :OAuth,
-      :cohort_id
-    )
-  end
+  # def idea_params
+  #   params.permit(
+  #     :title,
+  #     :pitch,
+  #     :problem,
+  #     :solution,
+  #     :audience,
+  #     :features,
+  #     :apis,
+  #     :oauth
+  #   )
+  # end
 end
