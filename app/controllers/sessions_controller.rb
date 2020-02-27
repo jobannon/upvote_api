7be2264ff1ee1260a44fa26d12abf5411a29ae54
Harrison Levin <harrison483@gmail.com>
2020-02-26 18:14:01 -0700

    ideas show page and env config

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,6 @@ class SessionsController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     user = User.create_user(auth_hash)
     session[:user_id] = user.id
-    redirect_to 'https://upvote-ideas-ui.herokuapp.com/cohorts/ideas'
+    redirect_to "#{ENV['FRONTEND']}/cohorts/ideas"
   end
 end
