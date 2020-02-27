1213fc0add6ef631aad0b8432fd1fc3c9c5ae6cf
Scott Regenthal <scottregenthal@gmail.com>
2020-02-26 21:01:01 -0700

    Add update and destroy actions to idea controller
    Co-Authored-By: Joshua O'bannon <jtobannon@gmail.com>

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::IdeasController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    user = current_user 
-    cohort = Cohort.find(user.cohort_id)
+    cohort = Cohort.find(current_user.cohort_id)
     render json: IdeaSerializer.new(cohort.ideas.sort_by { |idea| idea.vote_count }.reverse)
   end
 
@@ -13,10 +12,19 @@ class Api::V1::IdeasController < ApplicationController
 
   def create
     photo_url = IdeasPhotoGetter.new(params[:title]).get_photo
-    user = current_user 
-    Idea.create!(title: params[:title], pitch: params[:pitch], problem: params[:problem], solution: params[:solution], features: params[:features], audience: params[:audience], apis: params[:apis], oauth: params[:oauth], cohort_id: user.cohort_id, user_id: user.id, img_url: photo_url)
+    Idea.create!(title: params[:title], pitch: params[:pitch], problem: params[:problem], solution: params[:solution], features: params[:features], audience: params[:audience], apis: params[:apis], oauth: params[:oauth], cohort_id: current_user.cohort_id, user_id: current_user.id, img_url: photo_url)
+    redirect_to 'http://localhost:9292/cohorts/ideas'
+  end
 
-    redirect_to 'https://upvote-ideas-ui.herokuapp.com/cohorts/ideas'
+  def update
+    idea = Idea.find(params[:id])
+    idea.update!(title: params[:title], pitch: params[:pitch], problem: params[:problem], solution: params[:solution], features: params[:features], audience: params[:audience], apis: params[:apis], oauth: params[:oauth], cohort_id: current_user.cohort_id, user_id: current_user.id)
+    redirect_to 'http://localhost:9292/cohorts/ideas'
+  end
+
+  def destroy
+    Idea.find(params[:id]).destroy
+    redirect_to 'http://localhost:9292/cohorts/ideas'
   end
 
   #private
