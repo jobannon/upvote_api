67d8a43d010f2d6c52cdc1da12aa2567440cffd6
Harrison Levin <harrison483@gmail.com>
2020-02-27 15:51:38 -0700

    vote limite updated

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,7 +4,7 @@ class Vote < ApplicationRecord
 
 
   def self.vote_limit(user, idea_id)
-    count = Vote.where(user_id: user.id).where(cohort_id: user.cohort_id).where(idea_id: idea_id).count
+    count = Vote.where(user_id: user.id).where(cohort_id: user.cohort_id).count
     count < 3
   end
 
