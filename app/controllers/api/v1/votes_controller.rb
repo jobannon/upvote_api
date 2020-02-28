405795436166c4f677f188e6f290e47604dac3fa
Harrison Levin <harrison483@gmail.com>
2020-02-27 22:04:21 -0700

    update controller redirect

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -2,13 +2,14 @@ class Api::V1::VotesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
+    #require "pry"; binding.pry
     idea = Idea.find(params[:id])
     if Vote.vote_limit(current_user, idea.id)
       Vote.create!(user_id: current_user.id, idea_id: idea.id, cohort_id: current_user.cohort_id)
       idea.vote_count += 1
       idea.save
     end
-    redirect_to "#{ENV['FRONTEND']}/cohorts/ideas"
+    #redirect_to "#{ENV['FRONTEND']}/cohorts/ideas"
   end
 
   def show
